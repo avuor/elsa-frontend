78b3d5ebbcf0f7ebdc51f52188b09956bdfaa669
Tero Viljakainen <tero.viljakainen@gofore.com>
2021-10-10 18:58:57 +0300

    UOELSA-715: Korjaa RoleSpecificRoute -komponentti
    - Koska beforeRouteLeave hook toimii vain suoraan route componentissa mutta ei child componentissa, käytä ConfirmRouteExit mixiniä RoleSpecificRoute -komponentissa
    - Välitä confirmExit propseina routerilta RoleSpecificRoutelle
    - Välitä skipRouteExitConfirm eventti varsinaiselta sisältökomponentilta RoleSpecificRoutelle

--- src/mixins/confirm-route-exit.ts
+++ src/mixins/confirm-route-exit.ts
@@ -8,21 +8,6 @@ Component.registerHooks(['beforeRouteLeave'])
 export default class ConfirmRouteExit extends Vue {
   skipRouteExitConfirm = false
 
-  created() {
-    window.addEventListener('beforeunload', this.beforeWindowUnload)
-  }
-
-  beforeDestroy() {
-    window.removeEventListener('beforeunload', this.beforeWindowUnload)
-  }
-
-  beforeWindowUnload(e: BeforeUnloadEvent) {
-    if (!this.skipRouteExitConfirm) {
-      e.preventDefault()
-      e.returnValue = ''
-    }
-  }
-
   async beforeRouteLeave(to: any, from: any, next: any) {
     try {
       if (this.skipRouteExitConfirm || (await confirmExit(this))) {
